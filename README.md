329c0c79ab8f53db63d97ef3c91d554601bef7eb
WTassoux <tassoux@polytech.unice.fr>
2015-07-27 20:38:14 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TwitchInDota
+
+This script replaces the default chats in official TI5 streams with their Twitch counterpart.
---
+++ TwitchInDota.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name       TwitchInDota
+// @namespace  http://www.shunyweb.info
+// @version    1.0
+// @author     Shuny
+// @description  Replace Dota 2 default chatrooms with their Twitch counterpart.
+// @match      http://www.dota2.com/watch/76561198198486997
+// @match      http://www.dota2.com/watch/76561198207546029
+// @match      http://www.dota2.com/watch/76561198239691668
+// @copyright  2015+, Shuny
+// ==/UserScript==
+
+function createTwitchChatLink(twitchUser)
+{
+    return "http://www.twitch.tv/" + twitchUser + "/chat";
+}
+
+function getCurrentStreamId()
+{
+    return window.location.href.split(/[\/]+/).pop();
+}
+
+var streamIdToChatArray = {
+    "76561198198486997" : "dota2ti",
+    "76561198207546029" : "dota2ti_2",
+    "76561198239691668" : "dota2ti_3"
+};
+
+var chatIframe = document.getElementById('Chat');
+if (chatIframe !== null)
+{
+    var twitchChatToDisplay = streamIdToChatArray[getCurrentStreamId()];
+    chatIframe.src = createTwitchChatLink(twitchChatToDisplay);
+}
